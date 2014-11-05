946ade6c8df4a27095b804e612f3b69c00a08531
Robert McLeod <robert@penguinpower.co.nz>
2014-11-05 22:15:12 +1300

    refs #7 added a config class to handle loading options
    - sets some default options
    - loads options from a config file (if given)
    - raises error when config file not exist
    - lets command line options override config file options

--- lib/rackdis.rb
+++ lib/rackdis.rb
@@ -1,11 +1,13 @@
 require "redis"
 require "grape"
+require "yaml"
 require "rack/stream"
 require "redis/connection/synchrony"
 
 require "rackdis/version"
 require "rackdis/api"
 require "rackdis/redis_facade"
+require "rackdis/config"
 
 module Rackdis
   # Your code goes here...
---
+++ lib/rackdis/config.rb
@@ -0,0 +1,54 @@
+module Rackdis
+  class Config
+
+    DEFAULTS = {
+      port: 7380,
+      address: "0.0.0.0",
+      daemonize: false
+    }
+    
+    def initialize(opts)
+      @config = DEFAULTS.clone
+      process_options opts
+    end
+
+    def rack_options
+      {
+        Port: @config[:port],
+        Host: @config[:address],
+        daemonize: @config[:daemonize]
+      }
+    end
+    
+    private
+    
+    def process_options(opts)
+      load_from_file opts[:config]
+      
+      # We want to merge the command line opts
+      # overtop of the config file options but
+      # only if they are set because when they
+      # are not set they are nil which ends up
+      # wiping out all of the config
+      if opts.config?
+        merge_valid_opts(opts)
+      else
+        @config.merge!(opts)
+      end
+    end
+    
+    def load_from_file(file)
+      return false if file.nil?
+      raise ArgumentError, "Invalid config file: #{file}" unless File.exist? file
+      @config.merge!(YAML.load_file(file))
+    end
+
+    def merge_valid_opts(opts)
+      opts.to_hash.each do |key, value|
+        next if value.nil?
+        @config[key] = value
+      end
+    end
+
+  end
+end
