e085191fe64c66d0124bcdc6a752b0f6bdb820cf
Robert McLeod <robert@penguinpower.co.nz>
2014-11-08 04:04:49 +1300

    made valid_command! check that the command is safe

--- lib/rackdis/redis_facade.rb
+++ lib/rackdis/redis_facade.rb
@@ -30,6 +30,28 @@ module Rackdis
     private
     
     def valid_command!(cmd)
+      raise ArgumentError, "Unsupported command: #{command}" unless valid_command?
+    end
+    
+    def valid_command?(cmd)
+      safe_commands.include? cmd
+    end
+    
+    def safe_commands
+      [
+        :ping, :echo, :dbsize, :time,
+        :persist, :expire, :expireat, :ttl, :pexpire, :pexpireat, :pttl, :dump, :restore,
+        :set, :setex, :psetex, :setnx, :mset, :msetnx, :setrange, :append,
+        :get, :getset, :mget, :getrange,
+        :getbit, :setbit, :bitcount, :bitop, :bitpos,
+        :incr, :incrby, :incrbyfloat, :decr, :decrby, :del, :exists, :keys, :randomkey, :rename, :renamex, :sort, :type, :strlen, :scan,
+        :lpush, :lpushx, :rpush, :rpushx, :lrange, :lindex, :linsert, :llen, :lpop, :rpop, :rpoplpush, :lrem, :lset, :ltrim,
+        :sadd, :scard, :smembers, :sismember, :srem, :sinter, :sinterstore, :sdiff, :sdiffstore, :sunion, :sunionstore, :spop, :srandmember, :smove, :sscan,
+        :zcard, :zadd, :zincrby, :zrem, :zscore, :zrange, :zrevrange, :zrank, :zrevrank, :zremrangebyrank, :zrangebylex, :zrangebyscore, :zrevrangebyscore, :zremrangebyscore, :zcount, :zinterstore, :zunionstore, :zsan,
+        :hlen, :hset, :hsetnx, :hmset, :hget, :hmget, :hdel, :hexists, :hincrby, :hincrbyfloat, :hkeys, :hvals, :hgetall, :hscan,
+        :publish,
+        :pfadd, :pfcount, :pfmerge
+      ]
     end
 
     def respond(command, key, value=nil)
