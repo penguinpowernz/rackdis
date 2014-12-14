741017ade01068b640a35c72f44e2cd730dff4a6
Robert McLeod <robert@penguinpower.co.nz>
2014-12-14 15:57:54 +1300

    added support for more commands to the response_builder

--- lib/rackdis/response_builder.rb
+++ lib/rackdis/response_builder.rb
@@ -5,18 +5,41 @@ module Rackdis
     end
     
     def process(args, result)
-      response_hash = {
+      r = {
         command: @command.to_s.upcase,
         result: result
       }
       
       case @command
-      when :sinterstore
+      when :sinterstore, :sdiffstore, :sunionstore
+        r[:destination]  = args.first
+        r[:keys]         = args[1]
+      when :mget
+        r[:keys]         = args[0]
+        r[:pairs]        = {}
+        args.each_with_index do |key, index|
+          r[:pairs][key] = result[index]
+        end
+      when :sismember
+        r[:key]          = args.first
+        r[:member]       = args[1]
+      when :srandmember
+        r[:key]          = args.first
+        r[:count]        = args[1]
+      when :sdiff, :sunion, :sinter
+        r[:keys]         = args[0]
+      when :smove
+        r[:source]       = args[0]
+        r[:destination]  = args[1]
+        r[:member]       = args[2]
+      when :srem, :sadd
+        r[:key]          = args.first
+        r[:members]      = args[1]
       else
-        response_hash[:key] = args[0]
+        r[:key]          = args.first
       end
       
-      response_hash
+      r
     end
   end
 end
